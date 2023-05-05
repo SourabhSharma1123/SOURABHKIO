5ba03a50309f452680c3ac633f8cbcf75f51233c
Sourabh-sharma <89603381+SourabhSharma1123@users.noreply.github.com>
2023-05-05 11:22:30 +0530

    Add files via upload
    UPLOADED

---
+++ codeing.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 18 11:54:12 2023
+
+@author: ASUS
+"""
+
+
+from tkinter import *
+from tkinter import messagebox
+
+top=Tk()
+top.geometry("800x700")
+c=Canvas(top,bg="gray16",height=200,width=200)
+filename=PhotoImage(file="C:/Users/ASUS/Desktop/MachineLearning Folder/image_blr")
+background_label=Label(top,image=filename)
+background_label.place(x=0,y=0,relwidth=1,relheight=1)
+
+c.pack()
+top.mainloop()
